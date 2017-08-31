12624c38f5508b7a245081f4af3553946c27003f
irisyupingren <yuping.ren.iris@gmail.com>
2017-08-31 23:43:07 +0200

    Left to compare the time and actually separate the files

---
+++ MeredithTLF1MIREX2016/zzzdavid.bat
@@ -0,0 +1,3 @@
+rem for /r %%f in (*.txt) do java -jar MeredithTLF1MIREX2016.jar %%f %%f.tlf1
+for /r %%f in (*.txt) do java -jar MeredithTLPMIREX2016.jar %%f %%f.tlp
+for /r %%f in (*.txt) do java -jar MeredithTLRMIREX2016.jar %%f %%f.tlr
